c8f8ea22d2aab0bda206901983900cdb25dbbbee
Muhammad Islam Kamran <50517711+islamkamran@users.noreply.github.com>
2023-05-18 11:55:56 +0500

    OTP and Function

---
+++ functional.py
@@ -0,0 +1,7 @@
+import os
+os.system("clear")
+x=10
+def fun1():
+    print("Islam Kamran:", x)
+
+fun1()
---
+++ random_number.py
@@ -0,0 +1,6 @@
+from random import *
+
+# Each time this program will print an OTP different from previous one.
+print("Generating OTP")
+#sep is for seperate mean the spaces between the values is nill
+print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9), sep='')
