ade91b7e723da59a02c39475d5eeda5e1ff0549e
Muhammad Islam Kamran <50517711+islamkamran@users.noreply.github.com>
2023-05-19 11:33:50 +0500

    Keywords list and Bool

---
+++ bool.py
@@ -0,0 +1,7 @@
+# All the reserved words are in alphas
+# All key words except True False and None are in lower case.
+
+a = True #this is correct
+print(a)
+
+# b=true this is incorrect and will give error.
---
+++ list_keywords.py
@@ -0,0 +1,5 @@
+# to import and see all the list of reserved keywords
+
+import keyword
+
+print(keyword.kwlist)
